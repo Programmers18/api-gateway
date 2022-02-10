fd760baa1e51a9177e1aa914520c62b8cb0984c3
jonny1811 <jonny@quanto.la>
2022-02-10 15:48:53 -0300

    changes various

---
+++ src/categorias/categorias.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Get, Logger, Param, Post, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { ClientProxySmartRanking } from '../proxyrmq/client-proxy';
+import { AtualizarCategoriaDto } from './dtos/atualizar-categoria.dto';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
+
+@Controller('api/v1/categorias')
+export class CategoriasController {
+
+    private logger = new Logger(CategoriasController.name);
+
+    constructor(
+        private clientProxySmartRanking: ClientProxySmartRanking
+    ) {}
+
+    private clientAdminBackend = this.clientProxySmartRanking.getClientProxyAdminBackendInstance();
+
+    @Post('categorias')
+    @UsePipes(ValidationPipe)
+    criarCategoria(@Body() criarCategoriaDto: CriarCategoriaDto) {
+      this.clientAdminBackend.emit('criar-categoria', criarCategoriaDto);
+    }
+  
+    @Get('categorias')
+    consultarCategorias(@Query('idCategoria') _id: string): Observable<any> {
+      return this.clientAdminBackend.send('consultar-categorias', _id ? _id : '');
+    }
+  
+    @Put('categorias/:_id')
+    @UsePipes(ValidationPipe)
+    atualizarCategoria(
+      @Body() atualizarCategoriaDto: AtualizarCategoriaDto,
+      @Param('_id') _id: string,
+    ) {
+      this.clientAdminBackend.emit('atualizar-categoria', { id: _id, categoria: atualizarCategoriaDto });
+    }
+}
